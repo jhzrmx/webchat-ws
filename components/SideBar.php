4234f03681a629a575e028773899617782042878
jhzrmx <dj.jasper128@gmail.com>
2024-07-19 16:10:49 +0800

    UI changes

--- components/SideBar.php
+++ components/SideBar.php
@@ -27,7 +27,7 @@ function sideBar($display) {
 		<div id="sideBarMobile" class="block md:hidden h-dvh w-full p-4 absolute top-0 left-0">
 	        <div class="w-full h-full overflow-hidden bg-gray-300 rounded-lg p-3">
 	            <div class="flex items-center mb-3 mt-1">
-	            	<h2 class="w-full ml-2 text-2xl text-left font-bold">My Chats</h2>
+	            	<h2 class="w-full ml-2 text-2xl text-left font-bold">Chats</h2>
 	            	<button class="w-12 h-11 rounded-full hover:bg-gray-50 px-1">
 	            		<img class="w-10 h-10 justify-end" src="../img/icons/profile-circle-svgrepo-com.svg">
 	            	</button>
